643d661d4642115a59f35ac57ed6bc9669b8df3c
jovanna1616 <jovana.r@vivifyideas.com>
2018-04-26 21:04:03 +0200

    Added bootstrap.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <ContactsList/>
+    <button class="btn btn-primary">Hello bootstrap</button>
   </div>
 </template>
 
@@ -18,13 +18,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
